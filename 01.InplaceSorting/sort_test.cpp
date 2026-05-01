8abf3931cc090bff4ba1055f3ad29a2c7d823f25
perova-v <st141846@student.spbu.ru>
2026-05-01 18:15:57 +0300

    added sorting and tests

--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -1,11 +1,94 @@
 #include <gtest/gtest.h>
 
+#include <algorithm>
+#include <numeric>
+#include <random>
+#include <vector>
+
 // А вот тут collvalue.h не надо, берите просто инты
 #include "sorting.h"
 
-// Но проверьте, что сортировки таки работают...
 
-TEST(FooSuite, FooTest)
+TEST(BubbleSortTest, SortsEmptyVector)
 {
-    EXPECT_EQ(0, 0);
+    std::vector<int> data;
+    bubble_sort(data.begin(), data.end());
+    EXPECT_TRUE(data.empty());
+}
+
+TEST(BubbleSortTest, SortsSingleElement)
+{
+    std::vector<int> data = {7};
+    bubble_sort(data.begin(), data.end());
+    EXPECT_EQ(data, std::vector<int>({7}));
+}
+
+TEST(BubbleSortTest, SortsAlreadySorted)
+{
+    std::vector<int> data = {1, 2, 3, 4, 5};
+    bubble_sort(data.begin(), data.end());
+    EXPECT_EQ(data, std::vector<int>({1, 2, 3, 4, 5}));
+}
+
+TEST(BubbleSortTest, SortsReverseSorted)
+{
+    std::vector<int> data = {5, 4, 3, 2, 1};
+    bubble_sort(data.begin(), data.end());
+    EXPECT_EQ(data, std::vector<int>({1, 2, 3, 4, 5}));
+}
+
+TEST(BubbleSortTest, SortsWithDuplicates)
+{
+    std::vector<int> data = {8, 9, 4, 4, 7, 1, 1};
+    bubble_sort(data.begin(), data.end());
+    EXPECT_EQ(data, std::vector<int>({1, 1, 4, 4, 7, 8, 9}));
+}
+
+TEST(BubbleSortTest, SortsNegativeNumbers)
+{
+    std::vector<int> data = {0, -1, 7, -1, 3};
+    bubble_sort(data.begin(), data.end());
+    EXPECT_EQ(data, std::vector<int>({-1, -1, 0, 3, 7}));
+}
+
+TEST(QuickSortTest, SortsEmptyVector)
+{
+    std::vector<int> data;
+    quick_sort(data.begin(), data.end());
+    EXPECT_TRUE(data.empty());
+}
+
+TEST(QuickSortTest, SortsSingleElement)
+{
+    std::vector<int> data = {7};
+    quick_sort(data.begin(), data.end());
+    EXPECT_EQ(data, std::vector<int>({7}));
+}
+
+TEST(QuickSortTest, SortsAlreadySorted)
+{
+    std::vector<int> data = {1, 2, 3, 4, 5};
+    quick_sort(data.begin(), data.end());
+    EXPECT_EQ(data, std::vector<int>({1, 2, 3, 4, 5}));
+}
+
+TEST(QuickSortTest, SortsReverseSorted)
+{
+    std::vector<int> data = {5, 4, 3, 2, 1};
+    quick_sort(data.begin(), data.end());
+    EXPECT_EQ(data, std::vector<int>({1, 2, 3, 4, 5}));
+}
+
+TEST(QuickSortTest, SortsWithDuplicates)
+{
+    std::vector<int> data = {8, 9, 4, 4, 7, 1, 1};
+    quick_sort(data.begin(), data.end());
+    EXPECT_EQ(data, std::vector<int>({1, 1, 4, 4, 7, 8, 9}));
+}
+
+TEST(QuickSortTest, SortsNegativeNumbers)
+{
+    std::vector<int> data = {0, -1, 7, -1, 3};
+    quick_sort(data.begin(), data.end());
+    EXPECT_EQ(data, std::vector<int>({-1, -1, 0, 3, 7}));
 }
--- 01.InplaceSorting/sorting.h
+++ 01.InplaceSorting/sorting.h
@@ -6,16 +6,86 @@
 // Для swap
 #include "collvalue.h"
 
-// ----------------------------- Пузырёк (например) --------------------------------------
+#include <algorithm>
+#include <iterator>
+
+
+// ----------------------------- Пузырёк --------------------------------------
 template <typename Iterator>
 void bubble_sort(Iterator begin, Iterator end)
 {
+    if (begin == end)
+    {
+        return;
+    }
+
+    for (Iterator last = end; last != begin; --last)
+    {
+        bool swapped = false;
+
+        Iterator current = begin;
+        Iterator next = current;
+        ++next;
+
+        for (; next != last; ++current, ++next)
+        {
+            if (*next < *current)
+            {
+                std::iter_swap(current, next);
+                swapped = true;
+            }
+        }
+
+        if (!swapped)
+        {
+            break;
+        }
+    }
 }
 
-// --------------------------- QuickSort (например) ---------------------------------------
+// --------------------------- QuickSort ---------------------------------------
 template <typename Iterator>
 void quick_sort(Iterator begin, Iterator end)
 {
+    const auto size = std::distance(begin, end);
+    if (size < 2)
+    {
+        return;
+    }
+
+    Iterator left = begin;
+    Iterator right = end;
+    --right;
+
+    const auto pivot = *(begin + size / 2);
+
+    while (left <= right)
+    {
+        while (*left < pivot)
+        {
+            ++left;
+        }
+
+        while (pivot < *right)
+        {
+            --right;
+        }
+
+        if (left <= right)
+        {
+            std::iter_swap(left, right);
+            ++left;
+
+            if (right == begin)
+            {
+                break;
+            }
+            --right;
+        }
+    }
+
+    quick_sort(begin, right + 1);
+    quick_sort(left, end);
 }
 
 #endif // SORTING_H
